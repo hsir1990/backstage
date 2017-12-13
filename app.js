238a31a69c39018019db988fdbc6ef08620c7f11
rousong <rousong@7lk.com>
2017-12-13 19:57:40 +0800

    node生成器

---
+++ app.js
@@ -0,0 +1,47 @@
+// 引入node写的express框架
+var express = require('express'),
+    // // 'connect-flash'与'express-session'配合使用,flash方法储存值需要依赖express-sesion中，一般在使用redirect的时候使用
+    // flash = require('connect-flash'),
+    // session = require('express-session'),
+    path = reuqire('path'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    routes = require('./routes/main'),
+
+
+    //引用函数并初始化 
+    app = express();
+
+// // 引用flash方法
+// app.use(flash());
+
+// 设置标题栏小图标   path.join()让路径./practice.ico连接起来，__dirname的方法是表示当前路径，__filename表示代码所在的位置，module.filename：开发期间，该行代码所在的文件。__filename：始终等于 module.filename。__dirname：开发期间，该行代码所在的目录。process.cwd()：运行node的工作目录，可以使用  cd /d 修改工作目录。require.main.filename：用node命令启动的module的filename, 如 node xxx，这里的filename就是这个xxx。
+app.use(favicon(path.join(__dirname, 'practice.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'static')));
+
+
+routes(app);
+
+// 404异常处理
+app.use(function(req, res, next){
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// 打印错误信息
+app.use(function(err, req, res, next){
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    })
+})
+
+// 暴露app的接口
+module.exports = app;
---
+++ routes/main.js
@@ -0,0 +1,34 @@
+// 类似于jq的一个类库
+var _ = require('underscore'),
+
+    userMod = require('./modules/userMod'), //引入user（自己）模块
+    identityMode = require('./modules/identityMod'),//引入identity（身份）模块
+    menuMod = require('./modules/menuMod'),//引入menu（菜单）模块
+    competenceMod = require('./modules/competenceMod'),//引入competence（能力）模块
+    simpleMod = require('./modules/simpleMod'),//引入simple（简单）模块
+    config = {
+        get: {
+            '/set_v': simpleMode.setVersion,//设置静态资源版本号
+            '/get_v': simpleMod.getVersion//获取静态资源版本号
+        },
+        post: {
+            '/user/save': userMod.save,//保存用户信用
+            '/user/del': userMod.delete
+        }
+    };//路由配置
+
+module.exports = function(app){
+    // 分析路由配置对象，逐一处理
+    // subConfig--json的内容，method--get或post的方法
+    _.each(config, function(subConfig, method){
+        // url--自己定义的路径，func--- simpleMode.setVersion方法
+        _.each(subConfig, function(func, url){
+            // app.get('/set_v',userMod.save)
+            app[method](url, func);
+
+        })
+
+    })
+}
+    
+    
